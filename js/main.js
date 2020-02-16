100beaa713ac426119de322d411fa96d3ead08a6
Ruslan <slobodianiuk.ruslan.work@gmail.com>
2020-02-16 16:31:18 +0200

    Add JS & favicon

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 400) {
+			effectFirst();
+		}
+		if ($(this).scrollTop() > 2200) {
+			effectSecond();
+		}		
+	});
+
+	function effectFirst() {
+		$(".border_1").css("width","70%");
+	}
+	function effectSecond() {
+		$(".border_2").css("width","70%");
+	}
+});
